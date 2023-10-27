ac43516407a71cc0ef6aab8f23960b61743cb8bc
Rhishikesh12 <rhishikeshchaughule@gmail.com>
2023-10-27 12:45:24 +0530

    added project section

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,9 +1,122 @@
 /* eslint-disable no-unused-vars */
-// /* eslint-disable no-unused-vars */
 import React from "react";
+import styled from "styled-components";
+import { Projects } from "./data";
+import { InsertLinkTwoTone as LinkIcon } from "@mui/icons-material";
 
-export const Portfolio = () => {
-	return <div>About</div>;
+const Heading = styled.div`
+	text-align: center;
+	color: white;
+	font-size: 35px;
+	font-weight: bold;
+`;
+
+const PortfolioSection = styled.div`
+	display: flex;
+	flex-wrap: wrap;
+	justify-content: center;
+	margin-top: 50px;
+	margin-bottom: 50px;
+`;
+
+const PortfolioRow = styled.div`
+	display: flex;
+`;
+
+const PortfolioItem = styled.div`
+	width: 400px;
+	text-align: center;
+	margin: 20px;
+	position: relative;
+	cursor: pointer;
+	color: white;
+
+	img {
+		width: 100%;
+		height: 300px;
+		object-fit: cover;
+		border-radius: 15px;
+	}
+
+	.overlay {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		background: rgba(0, 0, 0, 0.7);
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		opacity: 0;
+		transition: opacity 0.3s;
+	}
+
+	.link-overlay {
+		position: absolute;
+		width: 50px;
+		height: 50px;
+		background: #fff;
+		border-radius: 50%;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+	}
+
+	.link-icon {
+		font-size: 25px;
+		color: black;
+		z-index: 1;
+		background: #fff;
+	}
+
+	&:hover .overlay {
+		opacity: 1;
+	}
+`;
+
+const Portfolio = () => {
+	const chunkedProjects = chunkArray(Projects, 3);
+
+	return (
+		<>
+			<Heading data-aos='fade-up'>Portfolio</Heading>
+			<PortfolioSection data-aos='fade-up'>
+				{chunkedProjects.map((row, rowIndex) => (
+					<PortfolioRow key={rowIndex}>
+						{row.map((project) => (
+							<PortfolioItem key={project.id}>
+								<img src={project.img} alt={project.title} />
+								<div className='overlay'>
+									<a
+										href={project.link}
+										target='_blank'
+										rel='noopener noreferrer'>
+										<div className='link-overlay'>
+											<LinkIcon className='link-icon' />
+										</div>
+									</a>
+								</div>
+							</PortfolioItem>
+						))}
+					</PortfolioRow>
+				))}
+			</PortfolioSection>
+		</>
+	);
 };
+
+// Helper function to chunk an array into smaller arrays
+function chunkArray(array, chunkSize) {
+	const chunkedArray = [];
+	for (let i = 0; i < array.length; i += chunkSize) {
+		chunkedArray.push(array.slice(i, i + chunkSize));
+	}
+	return chunkedArray;
+}
 
 export default Portfolio;
---
+++ src/components/data.js
@@ -0,0 +1,45 @@
+import project1Image from "../assets/project1.jpg";
+import project2Image from "../assets/project2.png";
+import project3Image from "../assets/project3.jpg";
+import project4Image from "../assets/project4.png";
+import project5Image from "../assets/project5.png";
+import project6Image from "../assets/project6.png";
+
+export const Projects = [
+	{
+		id: 1,
+		title: "Project 6",
+		link: "https://github.com/Rhishikesh12/Spectrum-UI",
+		img: project1Image,
+	},
+	{
+		id: 2,
+		title: "Project 6",
+		link: "https://assets.materialup.com/uploads/d0049bc2-257b-477f-a0dc-85874c3dafaa/preview.png",
+		img: project2Image,
+	},
+	{
+		id: 3,
+		title: "Project 6",
+		link: "https://assets.materialup.com/uploads/d0049bc2-257b-477f-a0dc-85874c3dafaa/preview.png",
+		img: project3Image,
+	},
+	{
+		id: 4,
+		title: "Project 6",
+		link: "https://assets.materialup.com/uploads/d0049bc2-257b-477f-a0dc-85874c3dafaa/preview.png",
+		img: project4Image,
+	},
+	{
+		id: 5,
+		title: "Project 6",
+		link: "https://assets.materialup.com/uploads/d0049bc2-257b-477f-a0dc-85874c3dafaa/preview.png",
+		img: project5Image,
+	},
+	{
+		id: 6,
+		title: "Project 6",
+		link: "https://assets.materialup.com/uploads/d0049bc2-257b-477f-a0dc-85874c3dafaa/preview.png",
+		img: project6Image,
+	},
+];
